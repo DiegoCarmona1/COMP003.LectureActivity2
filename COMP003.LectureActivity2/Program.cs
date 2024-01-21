fdb5b5410a008a5726347e5aff436274dda60a7a
Diego Carmona <diegocarmona@my.whccd.edu>
2024-01-20 23:13:06 -0800

    Initial Commit, Author, purpose, and reference added

--- COMP003.LectureActivity2/Program.cs
+++ COMP003.LectureActivity2/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace COMP003.LectureActivity2
+﻿/* 
+ * Author: Diego Carmona
+ * Course: COMP-003A
+ * Purpose: Lecture Activity for variables, type system, Math, and Console Properties
+ * reference: The C# Player's Guide (4e) by RB Whitaker
+ */
+
+namespace COMP003.LectureActivity2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
